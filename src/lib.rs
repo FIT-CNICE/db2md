1e7551b329b87b28f584a2f769ac4842fe0ceacc
Sizhe Liu <sizhe.liu@fit-foxconn.com>
2024-10-18 19:14:50 -0500

    lib: make process_data concurrent

--- src/lib.rs
+++ src/lib.rs
@@ -30,53 +30,57 @@ pub fn process_data(excel_path: &str,
   let field_map =
     yaml_parser::map_fields_to_columns(&fields, headers);
 
-  // process data rows iteratively
-  // let processed_rows = Arc::new(Mutex::new(0));
-  // let (sender, receiver) = mpsc::channel();
+  // Process data rows concurrently
+  let processed_rows = Arc::new(Mutex::new(0));
+  let (sender, receiver) = mpsc::channel();
   let threads: Vec<_> =
     rows.into_iter()
         .enumerate()
         .map(|(idx, row)| {
-          // let field_map = field_map.clone();
-          // let processed_rows = Arc::clone(&processed_rows);
-          // let sender = sender.clone();
-          // thread::spawn(move || {
-          // generate md string
-          let mut md_string = String::new();
-          md_gen::generate_markdown(&row, &field_map, &mut md_string);
-          // generate filename
-          let filename = format!("ccms-doc-{:03}.md", idx);
-          // write md file
-          if let Err(e) = std::fs::write(&filename, md_string) {
-            eprintln!("Failed to write file '{}': {}", filename, e);
-          }
-          // update progress
-          // let mut count = processed_rows.lock().unwrap();
-          // *count += 1;
-          // let progress = *count as f32 / tot_row_num;
-          // if let Err(e) = sender.send(progress) {
-          //   eprintln!("Failed to send progress update: {}",
-          // e); }
-          // })
+          let field_map = field_map.clone();
+          let processed_rows = Arc::clone(&processed_rows);
+          let sender = sender.clone();
+          thread::spawn(move || {
+            // generate md string
+            let mut md_string = String::new();
+            md_gen::generate_markdown(&row,
+                                      &field_map,
+                                      &mut md_string);
+            // generate filename
+            let filename = format!("ccms-doc-{:03}.md", idx);
+            // write md file
+            if let Err(e) = std::fs::write(&filename, md_string) {
+              eprintln!("Failed to write file '{}': {}", filename, e);
+            }
+            // update progress
+            let mut count = processed_rows.lock().unwrap();
+            *count += 1;
+            let progress = *count as f32 / tot_row_num;
+            if let Err(e) = sender.send(progress) {
+              eprintln!("Failed to send progress update: {}", e);
+            }
+          })
         })
         .collect();
 
-  // handle progress updates
-  // for progress in receiver {
-  //   if let Err(e) = progress_sender.send(progress) {
-  //     eprintln!("Failed to send progress update to GUI: {}", e);
-  //   }
-  // }
+  drop(sender);
 
-  // wait for all threads to complete
-  // for handle in threads {
-  //   handle.join().expect("Thread panicked");
-  // }
+  // Handle progress updates
+  for progress in receiver {
+    if let Err(e) = progress_sender.send(progress) {
+      eprintln!("Failed to send progress update to GUI: {}", e);
+    }
+  }
+
+  // Wait for all threads to complete
+  for handle in threads {
+    handle.join().expect("Thread panicked");
+  }
 
   Ok(())
 }
 
-// unit test for the process_data function
+// Unit test for the process_data function
 #[cfg(test)]
 mod tests
 {
